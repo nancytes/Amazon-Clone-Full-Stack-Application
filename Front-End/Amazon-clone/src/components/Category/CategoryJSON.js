d26fe8b26c2df6fc54936098fda487e83033e3e1
Nancy Tesfaye <nancy23tes@gmail.com>
2024-07-16 21:48:46 +0300

    category added

--- Front-End/Amazon-clone/src/components/Category/CategoryJSON.js
+++ Front-End/Amazon-clone/src/components/Category/CategoryJSON.js
@@ -1,1 +1,14 @@
-
+export const CategoryJSON = [
+  {
+    title: "Electronics",
+    img: "",
+  },
+  {
+    title: "Fashion",
+    img: "",
+  },
+  {
+    title: "Home & Kitchen",
+    img: "",
+  },
+];
