a13ef56c272ae8152d33c207272dd66daf365b06
Nancy Tesfaye <nancy23tes@gmail.com>
2024-07-22 20:39:25 +0300

    backend payment with firebase

---
+++ Amazon-clone/functions/index.js
@@ -0,0 +1,49 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Nancy",
+  });
+});
+
+
+app.post("/payment/create", async(req, res)=>{
+    const total = req.query.total;
+    if (total > 0) {
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: total,
+                currency: "usd",
+            });
+            res.status(200).json({
+                clientSecret: paymentIntent.client_secret,
+            });
+        } catch (error) {
+            console.error("Error creating payment intent:", error);
+            res.status(500).json({ error: "Failed to create payment intent" });
+        }
+    } else {
+        res.status(400).json({ error: "Total amount must be greater than zero" });
+    }
+    
+})
+
+exports.api = onRequest(app);
